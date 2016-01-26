daa74f3a061fdea1eda21f1b2db3f647b7df3cbd
AV1206 <jakub.lewek@digitalvirgo.pl>
2016-01-26 10:32:08 +0100

    Set app structure

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-angular.module('todoList.local', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+angular.module('todoList', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
 
-angular.module('todoList.local').config(function($stateProvider, $urlRouterProvider) {
-
-    /* Add New States Above */
-    $urlRouterProvider.otherwise('/home');
-
-});
-
-angular.module('todoList.local').run(function($rootScope) {
-
-    $rootScope.safeApply = function(fn) {
-        var phase = $rootScope.$$phase;
-        if (phase === '$apply' || phase === '$digest') {
-            if (fn && (typeof(fn) === 'function')) {
-                fn();
+angular.module('todoList').config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        views: {
+            '': {
+                templateUrl: 'partial/home/home.html',
+                controller: 'HomeCtrl'
             }
-        } else {
-            this.$apply(fn);
         }
-    };
+    });
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,172 @@
-//Experimental
+var fs = require('fs');
+var path = require('path');
 
-/*jslint node: true */
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var less = require('gulp-less');
-var gCheerio = require('gulp-cheerio');
-var ngHtml2js = require("gulp-ng-html2js");
-var ngmin = require('gulp-ngmin');
-var htmlmin = require('gulp-htmlmin');
-var cssmin = require('gulp-cssmin');
-var packagejson = require('./package.json');
-var streamqueue = require('streamqueue');
-var rimraf = require('rimraf');
-var rename = require('gulp-rename');
-var jshint = require('gulp-jshint');
-var jasmine = require('gulp-jasmine');
-var stylish = require('jshint-stylish');
-var domSrc = require('gulp-dom-src');
 
-var htmlminOptions = {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    removeAttributeQuotes: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    // removeRedundantAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true
-};
+// Load all gulp plugins automatically
+// and attach them to the `plugins` object
+var plugins = require('gulp-load-plugins')();
 
-gulp.task('clean', function() {
-    rimraf.sync('dist');
+// Temporary solution until gulp 4
+// https://github.com/gulpjs/gulp/issues/355
+var runSequence = require('run-sequence');
+
+var pkg = require('./package.json');
+var dirs = pkg['h5bp-configs'].directories;
+
+// ---------------------------------------------------------------------
+// | Helper tasks                                                      |
+// ---------------------------------------------------------------------
+
+gulp.task('archive:create_archive_dir', function () {
+    fs.mkdirSync(path.resolve(dirs.archive), '0755');
 });
 
-gulp.task('css', ['clean'], function() {
-    return gulp.src('app.less')
-        .pipe(less())
-        .pipe(cssmin({keepSpecialComments: 0}))
-        .pipe(rename('app.full.min.css'))
-        .pipe(gulp.dest('dist/'));
+gulp.task('archive:zip', function (done) {
+
+    var archiveName = path.resolve(dirs.archive, pkg.name + '_v' + pkg.version + '.zip');
+    var archiver = require('archiver')('zip');
+    var files = require('glob').sync('**/*.*', {
+        'cwd': dirs.dist,
+        'dot': true // include hidden files
+    });
+    var output = fs.createWriteStream(archiveName);
+
+    archiver.on('error', function (error) {
+        done();
+        throw error;
+    });
+
+    output.on('close', done);
+
+    files.forEach(function (file) {
+
+        var filePath = path.resolve(dirs.dist, file);
+
+        // `archiver.bulk` does not maintain the file
+        // permissions, so we need to add files individually
+        archiver.append(fs.createReadStream(filePath), {
+            'name': file,
+            'mode': fs.statSync(filePath).mode
+        });
+
+    });
+
+    archiver.pipe(output);
+    archiver.finalize();
+
 });
 
-gulp.task('js', ['clean'], function() {
-
-    var templateStream = gulp.src(['!node_modules/**','!index.html','!_SpecRunner.html','!.grunt/**','!dist/**','!bower_components/**','**/*.html'])
-        .pipe(htmlmin(htmlminOptions))
-        .pipe(ngHtml2js({
-            moduleName: packagejson.name
-        }));
-
-    var jsStream = domSrc({file:'index.html',selector:'script[data-build!="exclude"]',attribute:'src'});
-
-    var combined = streamqueue({ objectMode: true });
-
-    combined.queue(jsStream);
-    combined.queue(templateStream);
-
-    return combined.done()
-        .pipe(concat('app.full.min.js'))
-        .pipe(ngmin())
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
-
-
-    /* 
-        Should be able to add to an existing stream easier, like:
-        gulp.src([... partials html ...])
-          .pipe(htmlmin())
-          .pipe(ngHtml2js())
-          .pipe(domSrc(... js from script tags ...))  <-- add new files to existing stream
-          .pipe(concat())
-          .pipe(ngmin())
-          .pipe(uglify())
-          .pipe(gulp.dest());
-
-        https://github.com/wearefractal/vinyl-fs/issues/9
-    */
+gulp.task('clean', function (done) {
+    require('del')([
+        dirs.archive,
+        dirs.dist
+    ]).then(function () {
+        done();
+    });
 });
 
-gulp.task('indexHtml', ['clean'], function() {
-    return gulp.src('index.html')
-        .pipe(gCheerio(function ($) {
-            $('script[data-remove!="exclude"]').remove();
-            $('link').remove();
-            $('body').append('<script src="app.full.min.js"></script>');
-            $('head').append('<link rel="stylesheet" href="app.full.min.css">');
-        }))
-        .pipe(htmlmin(htmlminOptions))
-        .pipe(gulp.dest('dist/'));
+gulp.task('copy', [
+    'copy:.htaccess',
+    'copy:index.html',
+    'copy:jquery',
+    'copy:license',
+    'copy:main.css',
+    'copy:misc',
+    'copy:normalize'
+]);
+
+gulp.task('copy:.htaccess', function () {
+    return gulp.src('node_modules/apache-server-configs/dist/.htaccess')
+               .pipe(plugins.replace(/# ErrorDocument/g, 'ErrorDocument'))
+               .pipe(gulp.dest(dirs.dist));
 });
 
-gulp.task('images', ['clean'], function(){
-    return gulp.src('img/**')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/'));
+gulp.task('copy:index.html', function () {
+    return gulp.src(dirs.src + '/index.html')
+               .pipe(plugins.replace(/{{JQUERY_VERSION}}/g, pkg.devDependencies.jquery))
+               .pipe(gulp.dest(dirs.dist));
 });
 
-gulp.task('fonts', ['clean'], function(){
-    return gulp.src('bower_components/font-awesome/fonts/**')
-        .pipe(gulp.dest('dist/bower_components/font-awesome/fonts/'));
+gulp.task('copy:jquery', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+               .pipe(plugins.rename('jquery-' + pkg.devDependencies.jquery + '.min.js'))
+               .pipe(gulp.dest(dirs.dist + '/js/vendor'));
 });
 
-gulp.task('jshint', function(){
-    gulp.src(['!node_modules/**','!.grunt/**','!dist/**','!bower_components/**','**/*.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
+gulp.task('copy:license', function () {
+    return gulp.src('LICENSE.txt')
+               .pipe(gulp.dest(dirs.dist));
 });
 
-gulp.task('build', ['clean', 'css', 'js', 'indexHtml', 'images', 'fonts']);
+gulp.task('copy:main.css', function () {
 
-/* 
+    var banner = '/*! HTML5 Boilerplate v' + pkg.version +
+                    ' | ' + pkg.license.type + ' License' +
+                    ' | ' + pkg.homepage + ' */\n\n';
 
--specifying clean dependency on each task is ugly
-https://github.com/robrich/orchestrator/issues/26
+    return gulp.src(dirs.src + '/css/main.css')
+               .pipe(plugins.header(banner))
+               .pipe(plugins.autoprefixer({
+                   browsers: ['last 2 versions', 'ie >= 8', '> 1%'],
+                   cascade: false
+               }))
+               .pipe(gulp.dest(dirs.dist + '/css'));
+});
 
--gulp-jasmine needs a phantomjs option
-https://github.com/sindresorhus/gulp-jasmine/issues/2
+gulp.task('copy:misc', function () {
+    return gulp.src([
 
-*/
+        // Copy all files
+        dirs.src + '/**/*',
 
-/*
-    "gulp-dom-src": "~0.1.0",
-    "gulp-concat": "~2.1.7",
-    "gulp-uglify": "~0.2.1",
-    "gulp-cssmin": "~0.1.3",
-    "gulp-imagemin": "~0.1.5",
-    "gulp-less": "~1.2.2",
-    "gulp-cheerio": "~0.2.0",
-    "gulp-rename": "~1.2.0",
-    "gulp-ng-html2js": "~0.1.6",
-    "gulp-ngmin": "~0.1.2",
-    "gulp-htmlmin": "~0.1.2",
-    "gulp-jshint": "~1.5.0",
-    "gulp-jasmine": "~0.2.0",
-    "jshint-stylish": "~0.1.5",
-    "rimraf": "~2.2.6",
-    "streamqueue": "0.0.5",
-    "gulp": "~3.5.5"
-*/
+        // Exclude the following files
+        // (other tasks will handle the copying of these files)
+        '!' + dirs.src + '/css/main.css',
+        '!' + dirs.src + '/index.html'
+
+    ], {
+
+        // Include hidden files by default
+        dot: true
+
+    }).pipe(gulp.dest(dirs.dist));
+});
+
+gulp.task('copy:normalize', function () {
+    return gulp.src('node_modules/normalize.css/normalize.css')
+               .pipe(gulp.dest(dirs.dist + '/css'));
+});
+
+gulp.task('lint:js', function () {
+    return gulp.src([
+        'gulpfile.js',
+        dirs.src + '/js/*.js',
+        dirs.test + '/*.js'
+    ]).pipe(plugins.jscs())
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter('jshint-stylish'))
+      .pipe(plugins.jshint.reporter('fail'));
+});
+
+
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
+
+gulp.task('archive', function (done) {
+    runSequence(
+        'build',
+        'archive:create_archive_dir',
+        'archive:zip',
+    done);
+});
+
+gulp.task('build', function (done) {
+    runSequence(
+        ['clean', 'lint:js'],
+        'copy',
+    done);
+});
+
+gulp.task('default', ['build']);
